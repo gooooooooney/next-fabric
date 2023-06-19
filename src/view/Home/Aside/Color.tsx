c52f495a0f0af14bdc4966709dc360852c248051
张文光 <wenguangzhang@caixin.com>
2023-06-19 17:59:53 +0800

    add animate

--- src/view/Home/Aside/Color.tsx
+++ src/view/Home/Aside/Color.tsx
@@ -104,7 +104,7 @@ export const Color = () => {
     // state.canvas?.renderAll()
 
   }
-  return <div className="h-2xl absolute left-0 top-8 z-50 w-80 min-w-min overflow-y-auto rounded bg-white shadow-md animate-in">
+  return <div className="h-2xl absolute left-0 top-8 z-50 w-80 min-w-min overflow-y-auto rounded bg-white shadow-md animate-in slide-in-from-right-2">
     <div className="m-5">
       <div className="mb-3">Image</div>
 
